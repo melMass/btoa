3706bb2d1488ffd237714a4fcd68a9791ec3dcf7
Mel Massadian <melmassadian@gmail.com>
2022-01-04 01:52:20 +0100

    feat: 🎨 Material Init

--- ui/material.py
+++ ui/material.py
@@ -67,6 +67,9 @@ class ARNOLD_MATERIAL_PT_context_material(MaterialButtonsPanel, Panel):
             split.template_ID(space, "pin_id")
             split.separator()
 
+        if not mat.arnold.node_tree:
+            layout.operator("arnold.material_init")
+            return
 class ARNOLD_MATERIAL_PT_surface(MaterialButtonsPanel, Panel):
     bl_label = "Surface"
 
